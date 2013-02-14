519f9fad38b318464c4696aa359e8b6d668bbada
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-02-14 14:34:01 -0500

    Created Fire Alarm Model

---
+++ furnald_firedrill/libs/firealarms/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class FireAlarm(models.Model):
+
+    alarm_date = models.DateTimeField()
+
+    def __unicode__(self):
+            return self.alarm_date.isoformat()
